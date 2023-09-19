71cf276e309193a8931d484a6f86594a286d4263
Snerdify <srjbeliever0129@gmail.com>
2023-09-19 19:50:28 +0530

    pushed changes to main branch

---
+++ app/api/read/route.ts
@@ -0,0 +1,19 @@
+// we need the nextRequest and nextResponse for the route handling
+// create an instance of the pinecone client
+// get the query func from the utils
+// get the index
+
+import {NextRequest , NextResponse} from 'next/server'
+import {PineconeClient} from '@pinecone-database/pinecone'
+import {
+    queryPineconeVectorStoreandQueryLLM,
+} from '../../../utils'
+
+import { indexName } from '../../../config'
+
+
+export async function POST(req:NextRequest){
+    const body=await req.json()
+    const client = new PineconeClient()
+    
+}
---
+++ config.ts
@@ -0,0 +1,7 @@
+// config files 
+// contains indexname and timeout
+export const indexName = "my-test-index "
+
+export const timeout = 80000
+//timeout refers to how much time we give pinecone to initialize the index
+
