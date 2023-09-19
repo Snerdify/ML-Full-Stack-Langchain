a8c9bb6196748e459b6c9eaa93166cc1d1452d4c
Snerdify <srjbeliever0129@gmail.com>
2023-09-19 22:16:02 +0530

    tried solving the node_modules issue with next/server

--- app/api/read/route.ts
+++ app/api/read/route.ts
@@ -3,7 +3,7 @@
 // get the query func from the utils
 // get the index
 
-import {NextRequest , NextResponse} from 'next/server'
+import { NextRequest , NextResponse} from 'next/server' 
 import {PineconeClient} from '@pinecone-database/pinecone'
 import {
     queryPineconeVectorStoreandQueryLLM,
@@ -13,7 +13,18 @@ import { indexName } from '../../../config'
 
 
 export async function POST(req:NextRequest){
+    //new instance of the key
     const body=await req.json()
     const client = new PineconeClient()
+    // create a client
+    await client.init({
+        apiKey: process.env.PINECONE_API_KEY || '',
+        environment : process.env.PINECONE_ENVIRONMENT || ''
+    })
+
+
+
+
+
     
 }
